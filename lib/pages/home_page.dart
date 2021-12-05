6c776ad28c08ef0408118660a22806eb23e98fbd
RadzioN <radeknapora2003@gmail.com>
2021-12-05 18:03:18 +0100

    Dodawanie Realtime

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 //Plugins
+import 'package:auth/theme/colors.dart';
+import 'package:auth/widgets/sign_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 //Firebase Package
@@ -116,16 +118,99 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  final _test = TextEditingController();
+  final _focusTest = FocusNode();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _needs.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Text(
-          _needs[index].toString(),
-          //_needs[index]['Name'].toString(),
-        );
+    return GestureDetector(
+      onTap: () {
+        _focusTest.unfocus();
       },
+      child: Scaffold(
+        key: _scaffoldKey,
+        body: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: Center(
+                    child: ListView.builder(
+                      itemCount: _needs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Text(
+                          _needs[index].toString(),
+                          //_needs[index]['Name'].toString(),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SizedBox(
+            height: 120,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _test,
+                  focusNode: _focusTest,
+                  style: signTextFormField,
+                  decoration: CommonStyle.textFieldStyle(
+                    labelTextStr: "Nazwa",
+                  ),
+                  onChanged: (String? value) {},
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_test.text.isEmpty) {
+                            print("Pusto");
+                          } else {
+                            print(_test.text);
+                            ref.child('posts').push().set({
+                              'Name': _test.text,
+                            });
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: primaryColor,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 12.0,
+                          ),
+                        ),
+                        child: const Text(
+                          'Dodaj',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
