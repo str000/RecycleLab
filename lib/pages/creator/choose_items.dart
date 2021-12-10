ece13fa95e83b6edffde54f2508f95dc26408ac7
str000 <str000@protonmail.com>
2021-12-10 10:04:39 +0100

    Update choose_items.dart

--- lib/pages/creator/choose_items.dart
+++ lib/pages/creator/choose_items.dart
@@ -12,28 +12,48 @@ class ChooseItems extends StatefulWidget {
 }
 
 class _ChooseItems extends State<ChooseItems> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Scaffold(
         body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.only(bottom: 20, right: 20, top: 20, left: 20),
+            padding:
+                const EdgeInsets.only(bottom: 0, right: 20, top: 0, left: 20),
             child: Column(
               children: [
-                const Text(
-                  'Poduszka z folii',
-                  style: documentsText,
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        bottom: 20, right: 20, top: 20, left: 20),
+                    child: Text(
+                      'Wybierz przedmioty',
+                      style: documentsText,
+                    ),
+                  ),
                 ),
                 Stack(
                   alignment: Alignment.centerRight,
                   children: [
                     TextFormField(
                       style: signTextFormField,
+                      controller: myController,
                       decoration: CommonStyle.textFieldStyle(
                         labelTextStr: "np. butelka",
                       ),
-                      onChanged: (String? value) {},
+                      onChanged: (String? value) {
+                        setState(() {});
+                      },
                     ),
                   ],
                 ),
